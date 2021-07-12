f92a6481e2be3a783a2cf3276cbd98cd2acb41f5
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-12 11:47:25 +0530

    added:employe wage

---
+++ src/empwagebuilder/Employe.java
@@ -0,0 +1,19 @@
+package empwagebuilder;
+
+public class Employe {
+ public static void main (String[] args) {
+	 System.out.println("welcome to employe wage");
+	 
+	 int IS_FULL_TIME = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) %2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+	}
+}
+ 
+
