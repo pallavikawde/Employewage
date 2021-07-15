d12c2c2714304bba72541f5fc4bfd1930aa1f454
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-15 16:34:15 +0530

    Added:monthly wages

--- src/empwagebuilder/Employe.java
+++ src/empwagebuilder/Employe.java
@@ -1,25 +1,33 @@
 package empwagebuilder;
 
 import java.util.Scanner;
-
+// create class
 public class Employe {
+	//use main methode
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		// Constants
 		int IS_FULL_TIME = 1;
+		
+		//initialize value for month wages
+		 int WORKING_DAYS_MONTH=20;
+		 int i=0;
+		 int month_wage=0;
 		//Computation
+		 
 		double empCheck = Math.floor(Math.random() * 10) %2;
 		
+		//initialize value	for hr
+	
 		int wagePerHr = 20;
 		int FULL_DAY_HR = 8;
 		int PART_DAY_HR = 4;
-		
-		int dailyWage;
 		int wage = 0;
 		int IS_FULL_DAY = 1;
+		
 		double dayType = Math.floor(Math.random()*10)%2;
 		
-		
+		//use if else
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is present");
 			
@@ -44,6 +52,23 @@ public class Employe {
 		
 		switch(choice) {
 		case 1:
+		
+			for( i=1;i<=WORKING_DAYS_MONTH;i++)
+			{
+				dayType = Math.floor(Math.random()*10)%2;
+				
+					if (dayType == IS_FULL_DAY) { 
+						System.out.println(" Its a full day "+i );
+						month_wage = month_wage + (wagePerHr * FULL_DAY_HR);
+						
+					}
+					else 
+						System.out.println("Its a Part day " +i);
+						month_wage = month_wage + (wagePerHr * PART_DAY_HR);
+					}
+			
+		System.out.println("monthly wage is="+month_wage);
+			
 		
 		break;
 	
